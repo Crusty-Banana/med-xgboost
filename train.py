eb20671b2ef5c6f3b6c65411a93dc55de651670a
Crusty Banana <dodoankhac@gmail.com>
2024-08-21 07:44:16 +0000

    commit before migration

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
 import pandas as pd
-X_train = pd.read_csv('data/processed/test_processed_sample.csv')
+X_train = pd.read_csv('data/processed/train_processed_sample.csv', low_memory=False)
 print(X_train)
 print("Hellow")
